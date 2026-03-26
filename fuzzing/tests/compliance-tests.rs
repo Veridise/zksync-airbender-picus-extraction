81db54171d5dac7d5e6742192591d14d670a5c6c
Daniel Dominguez <daniel@veridise.com>
2026-03-26 11:55:43 +0100

    Update list of passing tests after run with prover

--- fuzzing/tests/compliance-tests.rs
+++ fuzzing/tests/compliance-tests.rs
@@ -123,53 +123,49 @@ fn test_unicorn<const N: usize, const M: usize>(#[case] binary: &[u8; N], #[case
     })
 }
 
-// Things that we need to do with the tests
-// - We need to define tests for the Zimop extension and, probably, write Rust programs that check
-// the tests.
-
 #[rstest]
-#[case::I_add(include_bin!(I!(add)), include_text!(I!(add)))]
-#[case::I_addi(include_bin!(I!(addi)), include_text!(I!(addi)))]
-#[case::I_and(include_bin!(I!(and)), include_text!(I!(and)))]
-#[case::I_andi(include_bin!(I!(andi)), include_text!(I!(andi)))]
-#[case::I_auipc(include_bin!(I!(auipc)), include_text!(I!(auipc)))]
-#[case::I_beq(include_bin!(I!(beq)), include_text!(I!(beq)))]
-#[case::I_bge(include_bin!(I!(bge)), include_text!(I!(bge)))]
-#[case::I_bgeu(include_bin!(I!(bgeu)), include_text!(I!(bgeu)))]
-#[case::I_blt(include_bin!(I!(blt)), include_text!(I!(blt)))]
-#[case::I_bltu(include_bin!(I!(bltu)), include_text!(I!(bltu)))]
-#[case::I_bne(include_bin!(I!(bne)), include_text!(I!(bne)))]
-#[case::I_jal(include_bin!(I!(jal)), include_text!(I!(jal)))]
-#[case::I_jalr(include_bin!(I!(jalr)), include_text!(I!(jalr)))]
-#[case::I_lb(include_bin!(I!(lb)), include_text!(I!(lb)))]
-#[case::I_lbu(include_bin!(I!(lbu)), include_text!(I!(lbu)))]
-#[case::I_lh(include_bin!(I!(lh)), include_text!(I!(lh)))]
-#[case::I_lhu(include_bin!(I!(lhu)), include_text!(I!(lhu)))]
-#[case::I_lui(include_bin!(I!(lui)), include_text!(I!(lui)))]
-#[case::I_lw(include_bin!(I!(lw)), include_text!(I!(lw)))]
-#[case::I_nop(include_bin!(I!(nop)), include_text!(I!(nop)))]
-#[case::I_or(include_bin!(I!(or)), include_text!(I!(or)))]
-#[case::I_ori(include_bin!(I!(ori)), include_text!(I!(ori)))]
-#[case::I_sb(include_bin!(I!(sb)), include_text!(I!(sb)))]
-#[case::I_sh(include_bin!(I!(sh)), include_text!(I!(sh)))]
-#[case::I_sll(include_bin!(I!(sll)), include_text!(I!(sll)))]
-#[case::I_slli(include_bin!(I!(slli)), include_text!(I!(slli)))]
-#[case::I_slt(include_bin!(I!(slt)), include_text!(I!(slt)))]
-#[case::I_slti(include_bin!(I!(slti)), include_text!(I!(slti)))]
-#[case::I_sltiu(include_bin!(I!(sltiu)), include_text!(I!(sltiu)))]
-#[case::I_sltu(include_bin!(I!(sltu)), include_text!(I!(sltu)))]
-#[case::I_sra(include_bin!(I!(sra)), include_text!(I!(sra)))]
-#[case::I_srai(include_bin!(I!(srai)), include_text!(I!(srai)))]
-#[case::I_srl(include_bin!(I!(srl)), include_text!(I!(srl)))]
-#[case::I_srli(include_bin!(I!(srli)), include_text!(I!(srli)))]
-#[case::I_sub(include_bin!(I!(sub)), include_text!(I!(sub)))]
-#[case::I_sw(include_bin!(I!(sw)), include_text!(I!(sw)))]
-#[case::I_xor(include_bin!(I!(xor)), include_text!(I!(xor)))]
-#[case::I_xori(include_bin!(I!(xori)), include_text!(I!(xori)))]
-#[case::M_divu(include_bin!(M!(divu)), include_text!(M!(divu)))]
-#[case::M_mul(include_bin!(M!(mul)), include_text!(M!(mul)))]
-#[case::M_mulhu(include_bin!(M!(mulhu)), include_text!(M!(mulhu)))]
-#[case::M_remu(include_bin!(M!(remu)), include_text!(M!(remu)))]
+#[case::I_add(include_bin!(I!(add)), include_text!(I!(add)))] // Passes
+#[case::I_addi(include_bin!(I!(addi)), include_text!(I!(addi)))] // Fails
+#[case::I_and(include_bin!(I!(and)), include_text!(I!(and)))] // Fails
+#[case::I_andi(include_bin!(I!(andi)), include_text!(I!(andi)))] // Passes
+#[case::I_auipc(include_bin!(I!(auipc)), include_text!(I!(auipc)))] // Passes
+#[case::I_beq(include_bin!(I!(beq)), include_text!(I!(beq)))] // Passes
+#[case::I_bge(include_bin!(I!(bge)), include_text!(I!(bge)))] // Passes
+#[case::I_bgeu(include_bin!(I!(bgeu)), include_text!(I!(bgeu)))] // Passes
+#[case::I_blt(include_bin!(I!(blt)), include_text!(I!(blt)))] // Passes
+#[case::I_bltu(include_bin!(I!(bltu)), include_text!(I!(bltu)))] // Passes
+#[case::I_bne(include_bin!(I!(bne)), include_text!(I!(bne)))] // Passes
+#[case::I_jal(include_bin!(I!(jal)), include_text!(I!(jal)))] // Passes
+#[case::I_jalr(include_bin!(I!(jalr)), include_text!(I!(jalr)))] // Passes
+#[case::I_lb(include_bin!(I!(lb)), include_text!(I!(lb)))] // Passes
+#[case::I_lbu(include_bin!(I!(lbu)), include_text!(I!(lbu)))] // Passes
+#[case::I_lh(include_bin!(I!(lh)), include_text!(I!(lh)))] // Passes
+#[case::I_lhu(include_bin!(I!(lhu)), include_text!(I!(lhu)))] // Passes
+#[case::I_lui(include_bin!(I!(lui)), include_text!(I!(lui)))] // Passes
+#[case::I_lw(include_bin!(I!(lw)), include_text!(I!(lw)))] // Passes
+#[case::I_nop(include_bin!(I!(nop)), include_text!(I!(nop)))] // Passes
+#[case::I_or(include_bin!(I!(or)), include_text!(I!(or)))] // Passes
+#[case::I_ori(include_bin!(I!(ori)), include_text!(I!(ori)))] // Passes
+#[case::I_sb(include_bin!(I!(sb)), include_text!(I!(sb)))] // Passes
+#[case::I_sh(include_bin!(I!(sh)), include_text!(I!(sh)))] // Passes
+#[case::I_sll(include_bin!(I!(sll)), include_text!(I!(sll)))] // Passes
+#[case::I_slli(include_bin!(I!(slli)), include_text!(I!(slli)))] // Passes
+#[case::I_slt(include_bin!(I!(slt)), include_text!(I!(slt)))] // Passes
+#[case::I_slti(include_bin!(I!(slti)), include_text!(I!(slti)))] // Fails
+#[case::I_sltiu(include_bin!(I!(sltiu)), include_text!(I!(sltiu)))] // Passes
+#[case::I_sltu(include_bin!(I!(sltu)), include_text!(I!(sltu)))] // Passes
+#[case::I_sra(include_bin!(I!(sra)), include_text!(I!(sra)))] // Passes
+#[case::I_srai(include_bin!(I!(srai)), include_text!(I!(srai)))] // Passes
+#[case::I_srl(include_bin!(I!(srl)), include_text!(I!(srl)))] // Passes
+#[case::I_srli(include_bin!(I!(srli)), include_text!(I!(srli)))] // Passes
+#[case::I_sub(include_bin!(I!(sub)), include_text!(I!(sub)))] // Passes
+#[case::I_sw(include_bin!(I!(sw)), include_text!(I!(sw)))] // Passes
+#[case::I_xor(include_bin!(I!(xor)), include_text!(I!(xor)))] // Passes
+#[case::I_xori(include_bin!(I!(xori)), include_text!(I!(xori)))] // Passes
+#[case::M_divu(include_bin!(M!(divu)), include_text!(M!(divu)))] // Passes
+#[case::M_mul(include_bin!(M!(mul)), include_text!(M!(mul)))] // Passes
+#[case::M_mulhu(include_bin!(M!(mulhu)), include_text!(M!(mulhu)))] // Passes
+#[case::M_remu(include_bin!(M!(remu)), include_text!(M!(remu)))] // Passes
 fn test_airbender<const N: usize, const M: usize>(
     #[case] binary: &[u8; N],
     #[case] text: &[u8; M],
